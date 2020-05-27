c37e46ef8416cee11fddf0890379860850297faa
parkseunghoon <parksh930@gmail.com>
2020-05-27 16:45:40 +0900

    no message

--- Interline_WorkReport/src/main/java/project/interline/report/MainController.java
+++ Interline_WorkReport/src/main/java/project/interline/report/MainController.java
@@ -1,5 +1,7 @@
 package project.interline.report;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +28,25 @@ public class MainController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
+	public String loginForm() {
 		
-		return "Login";
+		return "login";
 	}
 	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(String login_id, String login_pw, HttpSession session) {
+		
+		logger.debug("id:{},pw:{}", login_id,login_pw);
+		
+		return null;
+	}
+	
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public String logout(HttpSession session) {
+		logger.debug("logout");
+		session.removeAttribute("login_id");
+		session.invalidate();
+		
+		return "redirect:/";
+	}
 }
