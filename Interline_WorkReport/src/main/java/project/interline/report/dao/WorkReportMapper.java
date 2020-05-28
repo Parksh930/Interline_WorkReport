7abaf6914984b20f9b4419d5adb64c4649e6e168
navyya20 <navyya20@gmail.com>
2020-05-28 18:01:21 +0900

    리포트 DB에 저장

--- Interline_WorkReport/src/main/java/project/interline/report/dao/WorkReportMapper.java
+++ Interline_WorkReport/src/main/java/project/interline/report/dao/WorkReportMapper.java
@@ -1,5 +1,18 @@
 package project.interline.report.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import project.interline.report.vo.WorkReportVO;
+
 public interface WorkReportMapper {
+
+	int insertReport(WorkReportVO workReportVO);
+
+	ArrayList<Integer> isItNew(HashMap<String, Object> map);
+
+	int updateReport(WorkReportVO workReportVO);
+
+	int checkState(Integer reportNum);
 
 }
